4c71080b729a04f189dd3a67191be2589820d449
Agustin Gallardo <agustingh1583@gmail.com>
2020-06-03 12:35:39 -0500

    Chats en orden, manejos de scroll

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,17 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { ChatService } from '../../providers/chat.service';
+import { Mensaje } from '../../interfaces/mensaje.interface';
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html'
 })
-export class ChatComponent {
+export class ChatComponent implements OnInit {
 
+  public chats: Mensaje[] = [];
+  elemento: any;
   mensaje = '';
+
   constructor( public _cs: ChatService) {
    this._cs.cargarMensajes()
-    .subscribe();
+    .subscribe( resp => {
+      this.chats = resp;
+      setTimeout(() => {
+        this.elemento.scrollTop = this.elemento.scrollHeight;
+      }, 20);
+    });
    }
+
+  ngOnInit() {
+    this.elemento = document.getElementById('app-mensajes');
+  }
 
   enviar_mensaje() {
     console.log(this.mensaje);
